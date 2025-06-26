8070ed2165d9b174b4297b04cc5d2ef586d61e2f
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-06-26 20:38:44 +0300

    Update router.py

--- train images/app/router.py
+++ train images/app/router.py
@@ -1,24 +1,18 @@
 import logging
-from tasks import (
-    task_model_flux_pro,
-    task_model_flux_pro_ultra,
-    task_model_flux_dev,
-    task_model_ideogram_v2,
-    task_model_flux_schnell
-)  # Add all models here
+from tasks import task_model_stable_diffusion_v35_large             # Add all models here
 
 def run_model_task(**kwargs):
     model_name = kwargs.get("model_name")
     
-    if model_name == "fal-ai/flux-pro/v1.1":
-        task_model_flux_pro.train_model(**kwargs)
-    elif model_name == "fal-ai/flux-pro/v1.1-ultra":
+    if model_name == "fal-ai/stable-diffusion-v35-large":
+        task_model_stable_diffusion_v35_large.train_model(**kwargs)
+    elif model_name == "":
         task_model_flux_pro_ultra.train_model(**kwargs)
-    elif model_name == "fal-ai/flux/dev":
+    elif model_name == "":
         task_model_flux_dev.train_model(**kwargs)
-    elif model_name == "fal-ai/ideogram/v2":
+    elif model_name == "":
         task_model_ideogram_v2.train_model(**kwargs)
-    elif model_name == "fal-ai/flux/schnell":
+    elif model_name == "":
         task_model_flux_schnell.train_model(**kwargs)
     else:
         logging.error(f"Unsupported model: {model_name}")

654bce453df832f45f7b0b3235a3596d176337fa
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-07-07 16:18:44 +0300

    Update router.py

--- train images/app/router.py
+++ train images/app/router.py
@@ -1,19 +1,14 @@
 import logging
-from tasks import task_model_stable_diffusion_v35_large             # Add all models here
+from tasks import task_model_flux_lora_fast_training, task_model_hunyuan_video_lora_training             # Add all models here
 
 def run_model_task(**kwargs):
     model_name = kwargs.get("model_name")
     
-    if model_name == "fal-ai/stable-diffusion-v35-large":
-        task_model_stable_diffusion_v35_large.train_model(**kwargs)
-    elif model_name == "":
-        task_model_flux_pro_ultra.train_model(**kwargs)
-    elif model_name == "":
-        task_model_flux_dev.train_model(**kwargs)
-    elif model_name == "":
-        task_model_ideogram_v2.train_model(**kwargs)
-    elif model_name == "":
-        task_model_flux_schnell.train_model(**kwargs)
+    if model_name == "fal-ai/flux-lora-fast-training":
+        task_model_flux_lora_fast_training.train_model(**kwargs)
+    elif model_name == "fal-ai/hunyuan-video-lora-training":
+        task_model_hunyuan_video_lora_training.train_model(**kwargs)
+  
     else:
         logging.error(f"Unsupported model: {model_name}")
         raise ValueError(f"Unsupported model: {model_name}")

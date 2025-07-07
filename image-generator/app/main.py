542aa91e44db8ba2b1c931f6dc47e9d3d4515e0e
Hanan Bahtiti <55956102+hananbahtiti@users.noreply.github.com>
2025-07-07 15:18:17 +0300

    Update main.py

--- image-generator/app/main.py
+++ image-generator/app/main.py
@@ -17,7 +17,7 @@ class ImageRequest(BaseModel):
     params: Optional[Dict[str, Any]] = {}  # Dynamic parameters for each model
 
 # Connect to Redis
-redis_conn = Redis(host="redis", port=6379, decode_responses=True)
+redis_conn = Redis(host="image-redis", port=6379, decode_responses=True)
 
 # Queue
 queue = Queue("image_requests", connection=redis_conn)
